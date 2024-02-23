ec4b8036cf787b2ddc252168ad51a1eb9f67d6f9
GustavoAntony <gustavoa9@al.insper.edu.br>
2024-02-23 09:25:25 -0300

    alalala

---
+++ src/main/java/insper/ingressify/DiscoveryApplication.java
@@ -0,0 +1,9 @@
+package main.java.insper.ingressify;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
