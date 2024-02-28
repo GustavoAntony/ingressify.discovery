b7db04aea70f045bdd938e463837418ec62d1afb
eriksoaress <erikbtu2020@gmail.com>
2024-02-27 22:55:30 -0300

    package insper.ingressify

--- src/main/java/insper/ingressify/DiscoveryApplication.java
+++ src/main/java/insper/ingressify/DiscoveryApplication.java
@@ -1,8 +1,9 @@
-package main.java.insper.ingressify;
+package insper.ingressify;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @EnableEurekaServer
 @SpringBootApplication
